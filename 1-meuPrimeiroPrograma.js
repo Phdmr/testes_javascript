ffbf37ae49e68fb5b5669210197824920f9175b3
Pedro Rocha <33499845+Phdmr@users.noreply.github.com>
2021-12-01 22:55:36 -0300

    Primeiro programa somente em JS

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+function mostra(frase){
+    console.log(frase);
+}
+
+console.log("Meu primeiro programa: Trabalhando com variáveis");
+
+const idade = 24;
+
+mostra("A minha idade é "+idade);
+
+pule();
+
