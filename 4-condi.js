3e0982399ade73c0cdaff140f1909981805abffb
Pedro Rocha <33499845+Phdmr@users.noreply.github.com>
2021-12-06 18:01:12 -0300

    condicionais encadeadas

--- 4-condi.js
+++ 4-condi.js
@@ -7,22 +7,31 @@ const listaDeDestinos = new Array(
 
 const idadeComprador1 = 15;
 const idadeComprador2 = 21;
-
+estaCompanhado1 = true;
+estaCompanhado2 = false;
 listaDeDestinos.push(`Belém`);
 
 console.log(listaDeDestinos);
 
-function veriIdade(comp){
+function veriIdade(comp, estaCompanhado){
     if(comp>=18){
         console.log("Você pode adiquirir passagens"+
         " para os seguintes locais: "+listaDeDestinos);
+        
     }
     else{
-        console.log("Você mão tem idade para comprar "+
-        "as passagens");
+        if(estaCompanhado){
+            console.log("O seu responsável pode comparar passagens para "+
+            "os seguintes destinos: "+listaDeDestinos);
+            
+        }
+        else{
+            console.log("Você mão tem idade para comprar "+
+            "as passagens");
+        }
     }
 }
 
-veriIdade(idadeComprador1);
+veriIdade(idadeComprador1, estaCompanhado1);
 
-veriIdade(idadeComprador2);
+veriIdade(idadeComprador2, estaCompanhado2);
